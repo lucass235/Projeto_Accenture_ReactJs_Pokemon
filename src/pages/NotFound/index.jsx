a8bf74fdcf6cf1dfa6360de2920470926b8f5d48
m-carecho <milena.c@aluno.ifsp.edu.br>
2022-01-30 02:58:00 -0300

    feat - page 404 not found

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import style from './style.css'
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Página não encontrada</h1>
+    <div class="site">
+    <div class="sketch">
+        <div class="bee-sketch blue"></div>
     </div>
+
+<h1>404:
+    <small>Pokemon not found</small></h1>
+</div>
   );
 };

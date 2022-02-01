5c54accdcf03e0bae45371404f4f43bffe43ce97
Tatianydev <98428761+Tatianydev@users.noreply.github.com>
2022-01-31 22:21:52 -0300

    Feat - login
    HTML, referente ao formulario de login e cadastro.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,6 +4,18 @@ export default function Chart() {
   return (
     <div>
       <h1>Login</h1>
+      <div>
+        <form>
+          <label htmlFor="email">email</label>
+          <input type="mail" />
+          <label htmlFor="password">senha</label>
+          <input type="password" />
+             
+        </form>
+        <a href="\"><div>Esqueci minha senha</div></a>
+        <div><a href="\">Cadastre-se</a></div>
+
+      </div>
     </div>
   );
 };

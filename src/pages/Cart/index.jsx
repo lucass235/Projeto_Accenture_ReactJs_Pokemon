29bb4310ac564c7a0ddd2920309fafd0ff64a72b
Emerson-javax <emersonhenriquefelix2002@gmail.com>
2022-02-05 23:36:47 -0300

    Feat: Adicionando novo modelo carrinho

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,10 +1,128 @@
 import React from 'react';
+import Header from '../../components/Header'
+//import ImgPoke from '../../assets/pokemon.png';
+import { Styled } from './styles'
+import { useNavigate } from 'react-router-dom'
+import { FiPlusCircle, FiMinusCircle, FiTrash } from "react-icons/fi";
+//import { Style } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
-export default function Chart() {
+export default function Cart() {
+
+  const { cart } = useSelector(state => state.pokemon)
+  console.log(cart)
+
+  const navigate = useNavigate();
+
+  // function handleIncrementPokemon(pokemon) {
+  //   updatePokemonAmount({
+  //     pokemonId: pokemon.id,
+  //     amount: pokemon.amount + 1,
+  //   });
+  // }
+
+  // function handleDecrementPokemon(pokemon) {
+  //   updatePokemonAmount({
+  //     pokemonId: pokemon.id,
+  //     amount: pokemon.amount - 1,
+  //   });
+  // }
+
+  const total = cart.reduce((sumTotal, pokemon) => {
+    return sumTotal + pokemon.pokemon.price * pokemon.pokemon.amount;
+  }, 0);
+
+  function priceFormat(value) {
+    return Intl.NumberFormat(
+      'pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value);
+  }
+
+
+
   return (
-    <div>
-      <h1>Carrinho</h1>
-    </div>
+
+    <>
+      <Header />
+      <Styled.Container>
+        {cart.length === 0 ? (
+          <Styled.Content>
+            <h1>Seu carrinho está vazio =(</h1>
+            <button onClick={() => navigate("../home")}>
+              Voltar para Home
+            </button>
+          </Styled.Content>
+        ) : (
+          <Styled.Content>
+            <h1>Meu Carrinho</h1>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Produto</th>
+                    <th>Quantidade</th>
+                    <th>Preço</th>
+                    <th>Subtotal</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cart.map((pokemon, index) => (
+                    <Styled.Lucas key={index}>
+                      <td className="avatar-container">
+                        <img style={{ height: 128, width: 128 }} src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.pokemonId}.png`} alt='Imagem do Pokémon' />
+                      </td>
+                      <td className="product-price-container">
+                        <span>{pokemon.pokemon.name}</span>
+                      </td>
+                      <td className="product-amount-container">
+                        <button type='button'>
+                          <FiMinusCircle size="18px" color="black" />
+                        </button>
+                        <input type="text" id="amount" name="amount" readOnly value={pokemon.amount}  >
+                        </input>
+                        <button type='button'>
+                          <FiPlusCircle size="18px" color="black" />
+                        </button>
+                      </td>
+                      <td className='price-container'>
+                        <span>{pokemon.price}</span>
+                      </td>
+                      <td className="subtotal-container">
+                        <span></span>
+                      </td>
+                      <td className="delete-button-container">
+                        <button type='button'>
+                          <FiTrash size="22px" color='black' />
+                        </button>
+                      </td>
+                    </Styled.Lucas>
+                  ))}
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td>
+                      <div>
+                        <button >
+                          Finalizar Pedido
+                        </button>
+                        <div>
+                          <p>Total:</p>
+                          <strong>{priceFormat(total)}</strong>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </Styled.Content>
+        )}
+      </Styled.Container>
+
+    </>
   );
 };
-
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const Styled = {
+  Container: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    background: #bfbfbf;
+    h1 {
+      margin: 2rem 0;
+      font-family: 'Montserrat-Bold';
+      font-size: 18px;
+      color: "#717171";
+      padding: 0.5rem;
+    }
+    table{
+      margin: 0;
+      th {
+        font-size: 18px;
+      }
+      th, td {
+        padding: 1rem;
+        vertical-align: middle;
+        color: "#717171";
+      }
+      .foot-row{
+        padding: 2rem 0 0;
+        @media screen and (min-width: 320px) and (max-width: 768px) {
+          padding: 1.5rem;
+        }
+      }
+    }
+  `,
+
+  Content: styled.div`
+    width: 100%;
+    height: 100%;
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  TableWrapper: styled.div`
+    border-radius: 5px;
+    margin: 0 auto;
+    margin-bottom: 2rem;
+    width: 53rem;
+    padding: 2rem;
+    box-shadow: 5px 6px 10px rgba(0, 0, 0, 0.1);
+    background-color: #ffff;
+    @media screen and (min-width: 320px) and (max-width: 768px) {
+      width: 100%;
+      max-width: 35rem;
+      padding: 0;
+      margin-bottom: 2rem;
+      .table-head {
+        display: none;
+      }
+    }
+    @media screen and (min-width: 769px) and (max-width:1024px) {
+      width: 100%;
+      max-width: 50rem;
+    }
+  `,
+
+  Lucas: styled.tr`
+    height: 12rem;
+    font-size: 16px;
+    font-weight: 700;
+    border-bottom: 1px solid #DDDDDD;
+
+    
+  `
+}
